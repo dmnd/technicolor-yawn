c8437bcbfe31ca96ba09dae529318a98884cb6d2
Desmond Brand <dmnd@desmondbrand.com>
2013-02-24 17:47:24 -0800

    Cheeseshop stuff

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ Technicolor Yawn colours and filters the request logs from the Google App
 Engine dev server. It's pretty rough at the moment and still contains Khan
 Academy specific stuff.
 
+To install:
+
+    pip install technicolor-yawn
+
 Usage:
 
     dev_appserver.py --debug /path/to/your/app 2>&1 | technicolor-yawn
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='technicolor-yawn',
+    version='0.1.0',
+    author='Desmond Brand',
+    author_email='dmnd@desmondbrand.com',
+    scripts=['bin/technicolor-yawn'],
+    url='https://github.com/dmnd/technicolor-yawn',
+    license='LICENSE.txt',
+    description='Colours and filters request logs from'
+                ' the Google App Engine dev server',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "termcolor >= 1.1.0",
+    ],
+)
