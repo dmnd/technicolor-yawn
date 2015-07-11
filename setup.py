120a76515cfcd81ce172b115099d3f35fb02bd14
Desmond Brand <dmnd@desmondbrand.com>
2015-07-10 18:12:03 -0700

    :arrow_up: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='technicolor-yawn',
-    version='0.1.1',
+    version='0.2.0',
     author='Desmond Brand',
     author_email='dmnd@desmondbrand.com',
     scripts=['bin/technicolor-yawn'],
